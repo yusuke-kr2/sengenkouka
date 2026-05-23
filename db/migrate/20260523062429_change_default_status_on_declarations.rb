d8531f472022df47dfde9624db1f4e87bbd36d22
黒田 <ddki313kr2@gmail.com>
2026-05-23 17:09:51 +0900

    feat:宣言中のタブ追加 #83

---
+++ db/migrate/20260523062429_change_default_status_on_declarations.rb
@@ -0,0 +1,6 @@
+class ChangeDefaultStatusOnDeclarations < ActiveRecord::Migration[8.1]
+  def change
+    #declarationsテーブルのstatusカラムのデフォルト値をpendingからdeclaringに変更
+    change_column_default :declarations, :status, from: 0, to: 2
+  end
+end
