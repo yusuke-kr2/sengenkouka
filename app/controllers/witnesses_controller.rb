a7715cb6abe8a47edf04ea8e28f28fdb289fd23b
黒田 <ddki313kr2@gmail.com>
2026-06-10 07:31:12 +0900

    feat: 見届け一覧ページを追加 #147

--- app/controllers/witnesses_controller.rb
+++ app/controllers/witnesses_controller.rb
@@ -1,6 +1,13 @@
 class WitnessesController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @declarations = Declaration.joins(:witnesses)
+      .where(witnesses: { user_id: current_user.id })
+      .includes(:witnesses, user: { avatar_attachment: :blob })
+      .recent
+  end
+
   def create
     @declaration = Declaration.find(params[:declaration_id])
     current_user.witnesses.create(declaration: @declaration) unless @declaration.user == current_user
