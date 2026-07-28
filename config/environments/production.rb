577f20b9bd6760f122df1c946bf4abbd4fcae90a
黒田 <ddki313kr2@gmail.com>
2026-07-29 08:47:05 +0900

    feat: 独自ドメイン(sengen-kouka.com)のホスト設定を追加 #79

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,11 +84,12 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  #
+  config.hosts = [
+    "sengen-kouka.com",
+    "www.sengen-kouka.com",
+    /.*\.onrender\.com/
+  ]
+
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
