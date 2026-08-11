2866f4987d3039d64be7f7ef4bf7465b08268beb
黒田 <ddki313kr2@gmail.com>
2026-08-11 21:00:42 +0900

    style: rubocop lint修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable,
-           :omniauthable, omniauth_providers: [:google_oauth2]
+           :omniauthable, omniauth_providers: [ :google_oauth2 ]
 
     def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
